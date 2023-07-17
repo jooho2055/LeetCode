ed274a34517996c3f02b63c21dc00e9760408843
jooho <111959400+jooho2055@users.noreply.github.com>
2023-07-16 23:56:38 -0700

    Time: 1 ms (89.85%) | Memory: 41.2 MB (58.60%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,14 +1,22 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> allrows = new ArrayList<List<Integer>>();
-	ArrayList<Integer> row = new ArrayList<Integer>();
-	for(int i=0;i<numRows;i++)
-	{
-		row.add(0, 1);
-		for(int j=1;j<row.size()-1;j++)
-			row.set(j, row.get(j)+row.get(j+1));
-		allrows.add(new ArrayList<Integer>(row));
-	}
-	return allrows;
+       List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> innerList = new ArrayList<>();
+            int current = 1;
+
+            for(int j = 0; j < i + 1; j ++){
+                if((j == 0) || (j == i)){
+                    current = 1;
+                    innerList.add(current);
+                } else {
+                    current  = (result.get(i-1).get(j-1)) + (result.get(i-1).get(j));
+                    innerList.add(current);
+                }
+            }
+            result.add(innerList);
+        }
+        return result;
     }
 }
