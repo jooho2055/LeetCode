b315f337faa2d9505bb52f9a29bacf86351d7025
jooho <111959400+jooho2055@users.noreply.github.com>
2023-07-17 20:58:30 -0700

    Time: 15 ms (54.47%) | Memory: 47.6 MB (75.93%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,35 +1,17 @@
 class Solution {
     
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer>[] bucket = new List[nums.length + 1];
-        Map<Integer, Integer> Fmap = new HashMap<>();
-
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length; i++){
-            if(!Fmap.containsKey(nums[i])){
-                Fmap.put(nums[i], 1);
-            } else{
-                Fmap.put(nums[i], Fmap.get(nums[i]) + 1 );
-            }
-        }
-
-        for (int key : Fmap.keySet()) {
-		    int frequency = Fmap.get(key);
-		    if (bucket[frequency] == null) {
-		 	    bucket[frequency] = new ArrayList<>();
-		    }
-		bucket[frequency].add(key);
-	    }
-
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){ map.put(num, map.getOrDefault(num, 0) + 1); }
+        
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        for(int key : map.keySet()){ heap.add(key); }
+        
         List<Integer> list = new ArrayList<>();
-
-        for (int i = bucket.length - 1; i >= 0 && list.size() < k; i--){
-            if (bucket[i] != null) {
-			list.addAll(bucket[i]);
-		    }
+        for(int i = 0; i < k; i++){
+            list.add(heap.poll());
         }
-
+        
         int[] result = new int[list.size()];        
         for(int i = 0; i < list.size() ; i++){
             result[i] =list.get(i);            
