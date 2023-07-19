aea7fd798a7068337c7b07cb2ac956b4fe6a5da1
jooho <111959400+jooho2055@users.noreply.github.com>
2023-07-18 21:03:25 -0700

    Time: 20 ms (36.73%) | Memory: 44.6 MB (16.04%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Map<String , Integer> Emap = new HashMap<>();
+
+        for (int i = 0; i < emails.length; i++){
+            String[] splitString = emails[i].split("@");
+            splitString[0] = splitString[0].split("\\+")[0];
+            splitString[0] = splitString[0].replace(".", "");
+            String a = splitString[0] + "@" + splitString[1];
+
+            Emap.put(a, Emap.getOrDefault(a, 0) + 1);
+        }
+
+        return Emap.size();
+    }
+}
