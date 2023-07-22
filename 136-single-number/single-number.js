3d6e353122147efb3ebb8121285a2b25303a92c5
jooho <111959400+jooho2055@users.noreply.github.com>
2023-07-21 17:04:05 -0700

    Time: 58 ms (91.02%) | Memory: 44.6 MB (63.24%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,24 +3,10 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    const Tmap = new Map();
-
-    for(let i = 0; i < nums.length; i++){
-    
-        if(Tmap.has(nums[i])){
-            Tmap.set(nums[i], Tmap.get(nums[i]) + 1);
-        } else{
-            Tmap.set(nums[i], 1);
-        }
+    let num = 0;
+    for (let n of nums) {
+        num ^= n;
     }
-
-    
-
-    for (const [key, value] of Tmap) {
-        if(value == 1){
-            return key;
-        }
-    }
-
+    return num;
     
 };
