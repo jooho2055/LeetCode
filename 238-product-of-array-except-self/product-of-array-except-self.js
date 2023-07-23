dc235a40e2e6005709ca1aaed3224dc329681572
jooho <111959400+jooho2055@users.noreply.github.com>
2023-07-22 23:45:35 -0700

    Time: 110 ms (50.74%) | Memory: 52.7 MB (91.09%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let output = [];
+    let leftMult = 1;
+    let rightMult = 1;
+    for (let i=nums.length - 1; i >= 0; i--) {
+        output[i] = rightMult;
+        rightMult *= nums[i];
+    }
+    for (let j=0; j < nums.length; j++) {
+        output[j] *= leftMult;
+        leftMult *= nums[j];
+    }
+    return output;
+};
