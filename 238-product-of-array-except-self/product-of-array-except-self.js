c23dd7e072f4f77a7ccbe1560336a454e0c67d0e
jooho <111959400+jooho2055@users.noreply.github.com>
2023-07-22 23:54:49 -0700

    Time: 123 ms (25.26%) | Memory: 52 MB (96.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -4,15 +4,17 @@
  */
 var productExceptSelf = function(nums) {
     let output = [];
-    let leftMult = 1;
-    let rightMult = 1;
+    let prefix = 1;
+    let postfix = 1;
     for (let i=nums.length - 1; i >= 0; i--) {
-        output[i] = rightMult;
-        rightMult *= nums[i];
+        output[i] = postfix;
+        postfix *= nums[i];
+
+
     }
     for (let j=0; j < nums.length; j++) {
-        output[j] *= leftMult;
-        leftMult *= nums[j];
+        output[j] *= prefix;
+        prefix *= nums[j];
     }
     return output;
 };
