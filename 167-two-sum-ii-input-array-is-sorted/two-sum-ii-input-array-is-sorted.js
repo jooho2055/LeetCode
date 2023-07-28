8811e58f3a11f9deacd22d8fd0d0c3dd8b30e4a0
jooho <111959400+jooho2055@users.noreply.github.com>
2023-07-27 19:27:41 -0700

    Time: 54 ms (93.56%) | Memory: 42.7 MB (72.92%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -8,6 +8,7 @@ var twoSum = function(numbers, target) {
     let pointerOne = 0;
     let pointerTwo = numbers.length - 1;
 
+    
     while(pointerOne < pointerTwo){
         if((numbers[pointerOne] + numbers[pointerTwo]) === target){
             return [pointerOne + 1, pointerTwo + 1];
