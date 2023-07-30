0b2700c4916e1f6c0af5721d2507a8b6133732f2
jooho <111959400+jooho2055@users.noreply.github.com>
2023-07-29 19:16:04 -0700

    Time: 54 ms (82.34%) | Memory: 42.6 MB (32.81%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -5,13 +5,17 @@
  */
 var mergeAlternately = function(word1, word2) {
     
-    let result = '';
-    for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
-        if (i < word1.length) result += word1[i];
-        if (i < word2.length) result += word2[i];
-    }
-    return result;
-    
+    let i = 0;
+    let j = 0;
+    let res = [];
 
+    while(word1.length > i || word2.length > j){
+        res.push(word1[i])
+        res.push(word2[j])
+        i++;
+        j++
+    }
+
+    return res.join('')
 
 };
