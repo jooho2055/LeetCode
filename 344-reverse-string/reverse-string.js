eff8e4d3f1e7b46139e8bb0f90cf16e2b66babf5
jooho <111959400+jooho2055@users.noreply.github.com>
2023-07-31 23:07:44 -0700

    Time: 86 ms (79.39%) | Memory: 49.7 MB (27.96%) - LeetSync

---
+++ 344-reverse-string/reverse-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let pointerOne = 0;
+    let pointerTwo = s.length - 1;
+
+    while(pointerOne < pointerTwo){
+        let temp = "";
+
+        temp = s[pointerOne];
+        s[pointerOne] = s[pointerTwo];
+        s[pointerTwo] = temp;
+
+        pointerOne++;
+        pointerTwo--;
+    }
+};
