d72e4420b2d384dff4ae34eca0e30776ddc14a65
jooho <111959400+jooho2055@users.noreply.github.com>
2023-08-02 22:03:57 -0700

    Time: 71 ms (97.40%) | Memory: 46.3 MB (79.89%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,24 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let count = 0;
-    pOne = 0;
-    pTwo = nums.length - 1;
 
+    let pOne = 0;
+    
     for(let i = 0; i < nums.length; i++){
-        if(nums[i] === 0){
-            count++;
-            continue;
-        }
-        else{
-            nums[pOne] = nums[i];
-            pOne++;
+        if(nums[i] !== 0){
+            nums[pOne] = nums[i];    
+            pOne++;    
         }
     }
 
-    while(count!== 0){
-        nums[pTwo] = 0;
-        pTwo--;
-        count--;
+    for(let j = pOne; j < nums.length; j++){
+        nums[j] = 0;
     }
 };
