9100dafd1e455a054af8178cd1ba649ebbd908df
jooho <111959400+jooho2055@users.noreply.github.com>
2023-08-05 23:51:39 -0700

    Time: 71 ms (77.32%) | Memory: 49.4 MB (85.63%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -6,12 +6,11 @@ var maxArea = function(height) {
     
     let pOne = 0;
     let pTwo = height.length - 1;
-    let temp = 0;
     let result = 0;
 
     while(pOne < pTwo){
 
-        temp = (pTwo - pOne) * Math.min(height[pOne], height[pTwo]);
+        let temp = (pTwo - pOne) * Math.min(height[pOne], height[pTwo]);
 
         if(temp > result){
             result = temp;
@@ -25,5 +24,4 @@ var maxArea = function(height) {
     }
 
     return result;
-
 };
