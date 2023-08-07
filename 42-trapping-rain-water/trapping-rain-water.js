e7df2caa39ea69c14abf1b6a4bd56cca58933a73
jooho <111959400+jooho2055@users.noreply.github.com>
2023-08-06 19:09:20 -0700

    Time: 51 ms (97.74%) | Memory: 43.3 MB (50.70%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+
+  let l = 0;
+  let r = height.length - 1;
+
+  let lMax = 0;
+  let rMax = 0;
+  let res = 0;
+
+  while (l < r) {
+    lMax = Math.max(lMax, height[l]);
+    rMax = Math.max(rMax, height[r]);
+    res += lMax - height[l];
+    res += rMax - height[r];
+
+    height[l] < height[r] ? l++ : r--;
+  }
+
+  return res;
+};
