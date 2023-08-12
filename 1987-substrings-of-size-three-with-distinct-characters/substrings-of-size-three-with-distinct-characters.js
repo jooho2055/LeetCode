c957e6a7c33080f9244528f53bd9a669d03d6f35
jooho <111959400+jooho2055@users.noreply.github.com>
2023-08-11 18:42:58 -0700

    Time: 57 ms (77.57%) | Memory: 44.9 MB (19.86%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
@@ -4,40 +4,17 @@
  */
 var countGoodSubstrings = function(s) {
 
-    if(s.length < 3){
-        return 0;
-    }
+	let result = 0;
 
-    let result = 0;
-    let left = 0;
-    let right = 0;
-    let length = 3;
-    const map = new Map();
+	for (let index = 0; index < s.length - 2; index++) {
+		const subStr = [s[index], s[index + 1], s[index + 2]];
+		const set = new Set(subStr);
 
-    while(left < s.length - 2){
-
-        while(right - left + 1 <= length){
-            if(map.has(s[right])){
-                map.set(s[right], map.get(s[right]) + 1);
-            } else{
-                map.set(s[right], 1);
-            }
-
-            if(right - left === 2){
-                break;
-            }
-            right++;
-        }    
-
-        let maxNum = Math.max(...map.values());
-        if(maxNum === 1){
-            result++;
-        } 
-
-        map.clear();
-        left++;
-        right = left;
-    }
-
+        if(set.size === 3){
+            result += 1;
+        }
+		
+	}
+	
     return result;
 };
