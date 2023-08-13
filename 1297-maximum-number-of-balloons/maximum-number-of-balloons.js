77de15f9d160f1c708337e5a0acd9f1ce3ed86be
jooho <111959400+jooho2055@users.noreply.github.com>
2023-08-13 00:35:43 -0700

    Time: 49 ms (98.09%) | Memory: 43.8 MB (53.82%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.js
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.js
@@ -3,7 +3,11 @@
  * @return {number}
  */
 var maxNumberOfBalloons = function(text) {
- const map = { b: 0, a: 0, l: 0, o: 0, n: 0, };
+    if(text.length < 7){
+        return 0;
+    }
+
+    const map = { b: 0, a: 0, l: 0, o: 0, n: 0, };
     
     for (const l of text) {
         map[l]++; 
@@ -12,4 +16,5 @@ var maxNumberOfBalloons = function(text) {
     return Math.floor(
         Math.min(map.b, map.a, map.l / 2, map.o / 2, map.n)
     );
+
 };
