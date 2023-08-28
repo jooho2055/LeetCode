18e67b120b7105870e9c694618fcaca870d4721c
jooho <111959400+jooho2055@users.noreply.github.com>
2023-08-28 14:48:20 -0700

    Time: 57 ms (65.76%) | Memory: 42.7 MB (39.52%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+    let stack = [];
+
+    if(s.length % 2 === 1){
+        return false;
+    }  
+
+    for(let i = 0; i < s.length; i++){
+        if((s[i] == "(") | (s[i] == "[") | (s[i] == "{")){
+            stack.push(s[i]);
+        }
+
+        if(s[i] == ")"){
+            if(stack.pop() == "("){
+                continue;
+            } else{
+                return false;
+            }
+        }
+
+        if(s[i] == "]"){
+            if(stack.pop() == "["){
+                continue;
+            } else{
+                return false;
+            }
+        }
+
+        if(s[i] == "}"){
+            if(stack.pop() == "{"){
+                continue;
+            } else{
+                return false;
+            }
+        }
+    }
+
+    if(stack.length != 0){
+        return false;
+    }
+    return true;
+};
