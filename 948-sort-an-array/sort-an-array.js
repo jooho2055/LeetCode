1a02ad512add096ab08752fc622122edf69b2560
jooho <111959400+jooho2055@users.noreply.github.com>
2024-01-20 19:57:26 -0800

    Time: 227 ms (56.79%) | Memory: 75.7 MB (43.13%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    
+    if(nums.length <= 1){
+        return nums
+    }
+
+    let i = 0;
+    let mid = Math.floor(nums.length / 2);
+
+    let left = sortArray(nums.slice(i, mid));
+    let right = sortArray(nums.slice(mid));
+
+    
+    return merge(left, right )
+};
+
+
+ function merge(left, right){
+
+    const result = []; // k pointer
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    while(i < left.length && j < right.length){
+        if(left[i] > right[j]){
+            result.push(right[j]);
+            j++;
+        } else {
+            result.push(left[i]);
+            i++;
+        }
+        k++;
+    }
+
+    if(i !== left.length){
+        while(i < left.length){
+            result.push(left[i]);
+            i++;
+            k++;
+        }
+    }
+
+    if(j !== right.length){
+        while(j < right.length){
+            result.push(right[j]);
+            j++;
+            k++;
+        }
+    }
+     
+    return result
+}
