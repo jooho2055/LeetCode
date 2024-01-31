e3d61cdb3f32dd292a6213dc5ec657528a0f3d2c
jooho <111959400+jooho2055@users.noreply.github.com>
2024-01-31 15:06:28 -0800

    Time: 53 ms (58.85%) | Memory: 48.8 MB (15.98%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -9,17 +9,11 @@ var removeElement = function(nums, val) {
     let j = nums.length -1 ;
 
     while(i <= j){
-        const temp = nums[i];
-
+        
         if(nums[i] === val){
-            if(nums[j] === val){
-                j--;
-            }else{
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-                j--;
-            }
+            nums[i] = nums[j];
+            j--;
+    
         } else{
             i++;
         }
